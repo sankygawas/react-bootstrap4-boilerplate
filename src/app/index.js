0662b48d7eb9ccc993b9df4eb785e3c103072444
Sanket Arun Gawas <sanketgawas1993@gmail.com>
2017-11-04 11:45:44 +0530

    Add bootsrap 4

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,34 @@
-console.log("It Works");
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component{
+    render(){
+        return (
+        <div className="container">
+            
+            <div className="row">
+                <div className="md-col-6">
+                    <div className="jumbotron">
+                        <h1>First Component</h1>
+                        <p ><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                    </div>
+                    <div className="card text-white bg-primary mb-3" >
+                            <div className="card-header">Header</div>
+                            <div className="card-body ml-4">
+                                <h4 className="card-title">Primary card title</h4>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+           
+                <div className="md-col-6">
+                    <h1>Second Component</h1>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+render(<App/>,window.document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,34 @@ var config = {
                 query:{
                     presets:["react","es2015","stage-2"]
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ]
     }
